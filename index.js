1b9ee4f0c975166423cf891cc3cc57ac36c750e8
Danny Rodriguez <danny007111@hotmail.com>
2022-03-21 16:00:19 -0500

    Uploading changes

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block){ 
+    const location = 42
+    if(block < location){
+        return location - block;
+    }else{
+        return block - location;
+    };
+  };
+
+  function distanceFromHqInFeet(block){
+      const bToF = 264
+    return distanceFromHqInBlocks(block) * bToF;
+  };
+
+  function distanceTravelledInFeet(hey){
+      hey;
+  };
