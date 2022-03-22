178b86751011c31b38d3b6a9f2bbce234a2d50dc
Danny Rodriguez <danny007111@hotmail.com>
2022-03-22 17:57:15 -0500

    Working on the last question

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(block){ 
-    const location = 42
-    if(block < location){
-        return location - block;
-    }else{
-        return block - location;
+    let location = 42;
+    return Math.abs(block - location);
+    // if(block < location){
+    //     return location - block;
+    // }else{
+    //     return block - location;
+    // };
+};
+
+function distanceFromHqInFeet(block){
+    let bToF = 264;
+return distanceFromHqInBlocks(block) * bToF;
+};
+
+function distanceTravelledInFeet(block1, block2){
+    let bToF1 = 264;
+    return Math.abs(block1 - block2) * bToF1;
+    // if(block1 < block2){
+    //     const fblock = block2 - block1;
+    //     return fblock * bToF1;
+    // }else{
+    //     const fblock = block1 - block2;
+    //     return fblock * bToF1;
+    // };
+};
+
+function calculatesFarePrice(start, destination){
+    let rate = 0.02;
+    let distance = distanceTravelledInFeet(start, destination);
+    while(distance < 2500){
+        if(distance <= 400){
+            return 0;
+        }else if(distance > 400){
+            return (distance * rate) - 8 ;
+        }else if(distance > 2000){
+            return 25;
+        };
     };
-  };
-
-  function distanceFromHqInFeet(block){
-      const bToF = 264
-    return distanceFromHqInBlocks(block) * bToF;
-  };
-
-  function distanceTravelledInFeet(hey){
-      hey;
-  };
+    if(distance > 2500){
+    return console.log("cannot travel that far");
+    }
+};
